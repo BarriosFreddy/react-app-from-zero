9b1bec5c50ed0463971373cbfae12d1d19875546
Freddy Barrios <freddybarrios.077@gmail.com>
2020-06-08 23:03:21 -0500

    Adding validations

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -16,7 +16,11 @@ const Home = ({ mylist, trends, originals}) => {
         <Categories title="Mi Lista">
           <Carousel>
             {mylist.map(item =>
-              <CarouselItem key={item.id} {...item} />
+              <CarouselItem 
+              key={item.id} 
+              {...item}
+              isList
+               />
             )}
           </Carousel>
         </Categories>
